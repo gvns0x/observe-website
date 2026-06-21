14780854582ac05c6b85f1c913930beeadb873c3
gvns0x <gvns0x@gmail.com>
2026-06-21 09:38:58 +0530

    add explanations

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -3,17 +3,25 @@ import itemsData from './introItems'
 
 export default function Intro({ scrollNumber, startPosition, step }) {
 
+    // Get the index of the item to show, based on the current scrollNumber
+    // Does not account for if the index is above the length of the array of items
+    // To consider that, we check the minimum number in itemIndex 👇
     const currentIndex: number = Math.floor((scrollNumber - startPosition) / step)
 
+    // If the scrollNumber is indicating an index bigger than the length of the array of items
+    // then use the index for the last item of the array of items.
+    // This means that if we scroll a lot, always render the last item of the array.
     const itemIndex = Math.min(currentIndex, itemsData.length - 1)
 
     if (currentIndex < 0) {
         return null
     }
 
+    const Item = itemsData[itemIndex]
+
     return (
         <>
-            {itemsData[itemIndex]}
+            {<Item active="true" itemIndex={itemIndex} scrollNumber={scrollNumber}/>}
         </>
     )
 }
--- src/components/intro/introItems.tsx
+++ src/components/intro/introItems.tsx
@@ -2,20 +2,23 @@ import styles from './Intro.module.css'
 import styled from 'styled-components'
 
 const IntroElement = styled.p`
-    color:blue
+    color: ${props => props.active ? "red" : "blue"};
+    filter: blur(${props => props.scrollNumber > 300 ? "24" : "0" };
+
+
 `
 
 const introItems = [
-    <IntroElement>I want to know</IntroElement>,
-    <IntroElement >if this actually reads</IntroElement>,
-    <IntroElement>well, because</IntroElement>,
-    <IntroElement>I'm not so sure</IntroElement>,
-    <IntroElement>that as I scroll</IntroElement>,
-    <IntroElement>it really works</IntroElement>,
-    <IntroElement>7</IntroElement>,
-    <IntroElement>8</IntroElement>,
-    <IntroElement>9</IntroElement>,
-    <IntroElement>10</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement itemIndex={itemIndex} scrollNumber={scrollNumber}>I want to know</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>if this actually reads</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>well, because</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>I'm not so sure</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>that as I scroll</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>it really works</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>7</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>8</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>9</IntroElement>,
+    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>10</IntroElement>,
 ]
 
 export default introItems
