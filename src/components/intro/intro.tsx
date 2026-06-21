965095bb35ed1877acf783714782db280ce162bc
gvns0x <gvns0x@gmail.com>
2026-06-21 10:31:42 +0530

    add opacity animation

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -17,11 +17,22 @@ export default function Intro({ scrollNumber, startPosition, step }) {
         return null
     }
 
+    const blurScrollStart = itemIndex * step + (step + startPosition - 20)
+    const blurScrollStop = itemIndex * step + (step + startPosition)
+    console.log(scrollNumber >= blurScrollStart, "blur is ", blurScrollStart," and scroll is ", scrollNumber)
+
+    // Item is what we'll render. It's always the item in the itemIndex in the array
     const Item = itemsData[itemIndex]
 
     return (
         <>
-            {<Item active="true" itemIndex={itemIndex} scrollNumber={scrollNumber}/>}
+            {<Item
+                active="true"
+                itemIndex={itemIndex}
+                blurScrollStart={blurScrollStart}
+                blurScrollStop={blurScrollStop}
+                scrollNumber={scrollNumber}
+            />}
         </>
     )
 }
--- src/components/intro/introItems.tsx
+++ src/components/intro/introItems.tsx
@@ -2,23 +2,22 @@ import styles from './Intro.module.css'
 import styled from 'styled-components'
 
 const IntroElement = styled.p`
-    color: ${props => props.active ? "red" : "blue"};
-    filter: blur(${props => props.scrollNumber > 300 ? "24" : "0" };
-
-
+    opacity: ${props => props.scrollNumber > props.blurScrollStart ? "0" : "1" };
+    
+    transition: opacity .3s;
 `
 
 const introItems = [
-    ({active, scrollNumber, itemIndex}) => <IntroElement itemIndex={itemIndex} scrollNumber={scrollNumber}>I want to know</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>if this actually reads</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>well, because</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>I'm not so sure</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>that as I scroll</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>it really works</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>7</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>8</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>9</IntroElement>,
-    ({active, scrollNumber, itemIndex}) => <IntroElement scrollNumber={scrollNumber}>10</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>I want to know</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>if this actually reads</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>well, because</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>I'm not so sure</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>that as I scroll</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>it really works</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>7</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>8</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>9</IntroElement>,
+    ({active, scrollNumber, itemIndex, blurScrollStart, blurScrollStop}) => <IntroElement blurScrollStart={blurScrollStart} scrollNumber={scrollNumber}>10</IntroElement>,
 ]
 
 export default introItems
