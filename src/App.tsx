65af8234555f03d1559630f85e5a54373d573cf0
gvns0x <gvns0x@gmail.com>
2026-07-04 09:09:24 +0530

    first test

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
     // Show next item after 200px scroll
     step: 220,
     // Number of pixels through which we'll scroll and see the blur
-    blurInterval: 50
+    blurInterval: 40
   }
 
   return (
