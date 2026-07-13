3c0c4118cd7b769cfeedc59ba4cb86c97f1409fd
gvns0x <gvns0x@gmail.com>
2026-07-13 08:14:56 +0530

    add hero content

--- src/pages/Temporary.tsx
+++ src/pages/Temporary.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import Wrapper from '../components/Wrapper/Wrapper'
+
 import HeroWatch from '../assets/herowatch.png'
 
+
+const HeroTitle = styled.h1`
+    line-height: 95%;
+    background: linear-gradient(to bottom, #5B564A, #3C362C);
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+`
+
+const HeroBlock = styled.div`
+    display:flex;
+    flex-direction:row;
+    gap: 32px;
+    justify-content: center;
+    align-items: center;
+    font-size:16px;
+`
+
 function Temporary() {
+
+    const heroImageStyle = {
+        width: "24vh",
+    }
+
     return(
         <Wrapper>
-            <p>Hey, there</p>
+        <img src={HeroWatch} style={heroImageStyle}/>
+            <HeroTitle>Apple Watch vibration guided meditation in 3 phases.</HeroTitle>
+            <HeroBlock>
+                <p>Breath</p>
+                <p>Body</p>
+                <p>Gratitude</p>
+            </HeroBlock>
         </Wrapper>
     )
 }
