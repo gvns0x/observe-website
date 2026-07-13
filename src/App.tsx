d64257c8b3592dd36abbcacfe7deb64f5b4c9779
gvns0x <gvns0x@gmail.com>
2026-07-13 08:59:40 +0530

    remove unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import Temporary from './pages/Temporary'
 
